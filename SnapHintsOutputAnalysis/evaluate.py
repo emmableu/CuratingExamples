1cb5a96e2c45c3dfde5544d1f94aba0d657fdf9f
wwang33 <wwang33@ncsu.edu>
2020-05-26 11:00:22 -0400

    erroneous hyperparameter tuning

--- SnapHintsOutputAnalysis/evaluate.py
+++ SnapHintsOutputAnalysis/evaluate.py
@@ -1,11 +1,11 @@
 import sys
 sys.path.append('/Users/wwang33/Documents/IJAIED20/CuratingExamples/Datasets')
 from Dataset import *
-
+import copy
 
 
 # behavior_labels = ["keymove", "jump", "cochangescore", "movetomouse", "moveanimate", "costopall"]
-behavior_labels = ["cochangescore"]
+behavior_labels = ["jump"]
 # behavior_labels = ["costopall", "movetomouse",  "jump", "cochangescore","keymove"]
 
 def get_yes_no(data, yes_no_group):
@@ -14,17 +14,31 @@ def get_yes_no(data, yes_no_group):
     yes_x = np.array(yes_x).transpose()
     return yes_x
 
-def get_x_y_train_snaphints(snaphints_dir, select = True):
+def get_x_y_train_snaphints(snaphints_dir):
     data = pd.read_csv(snaphints_dir + "train.csv", index_col = 0)
     yes_x = get_yes_no(data, "yes")
     no_x = get_yes_no(data, "no")
     x = np.vstack((yes_x, no_x))
     y = np.hstack((np.array([1]*yes_x.shape[0]), np.array([0]*no_x.shape[0])))
-    if select:
-        selected_features = get_selected_feature_index(snaphints_dir, 0.3, 0.3, 0.8)
-        # selected_features = get_selected_feature_index(snaphints_dir, jd_diff, jd_yes, confidence)
-        x = x[:,selected_features]
-    return x, y
+
+    diff_params = [0.1, 0.2, 0.25, 0.3, 0.35, 0.4, 0.5]
+    yes_params = [0.3, 0.4, 0.5]
+    yes_params = [0.3]
+    confidence_params = [0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95]
+    confidence_params = [0.6]
+    feature_grids = [(x, y, z) for x in diff_params for y in yes_params for z in confidence_params]
+    max_f1 = 0
+    x_orig = copy.deepcopy(x)
+    for feature_grid in feature_grids:
+        print(feature_grid)
+        selected_features = get_selected_feature_index(snaphints_dir, feature_grid[0], feature_grid[1], feature_grid[2])
+        x = x_orig[:,selected_features]
+        f1 = svm_linear.model_cross_val_predict(x, y)['f1']
+        if f1 >= max_f1:
+            best_selected_features = selected_features
+            selected_feature_grid = feature_grid
+            max_f1 = f1
+    return x_orig[:,best_selected_features], y, best_selected_features, selected_feature_grid, max_f1
 
 
 def get_x_y_test_snaphints(snaphints_dir, selected_features):
@@ -83,8 +97,10 @@ def snaphints_crossvalidation():
             for fold in range(10):
                 snaphints_dir = "/Users/wwang33/Documents/SnapHints/data/csc110/fall2019project1/submitted/" \
                                 + behavior + "/cv/fold" + str(fold) + "/SnapHints" + method + "/"
-                X_train, y_train = get_x_y_snaphints(snaphints_dir, "train")
-                X_test, y_test = get_x_y_snaphints(snaphints_dir, "test")
+                X_train, y_train, best_features, best_grid, best_f1 = get_x_y_train_snaphints(snaphints_dir)
+                X_test, y_test = get_x_y_test_snaphints(snaphints_dir, best_features)
+                print("bestfeature: ", best_features)
+                print("bestf1: ", best_f1)
                 y_test_total.extend(y_test)
                 y_pred  = lr.get_y_pred(X_train,  X_test, y_train)
                 print(y_pred)
